581e0cbc50bd57b7fd6b2b022571cad3390f392e
SMIKLooK <hdhddby249@gmail.com>
2023-08-10 23:54:46 +0300

    Добавлены кнопки для перехода на фрагменты, реализован 1 фрагмент

--- app/src/main/java/com/example/dogapi/MainActivity.kt
+++ app/src/main/java/com/example/dogapi/MainActivity.kt
@@ -2,18 +2,9 @@ package com.example.dogapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
-import android.view.View
-import android.widget.Toast
-import com.example.dogapi.call.Callhttp
 import com.example.dogapi.databinding.ActivityMainBinding
-import com.squareup.picasso.Callback
-import com.squareup.picasso.Picasso
-import java.lang.Exception
-
 
 private lateinit var binding: ActivityMainBinding
-private lateinit var imageLoader: Callhttp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,36 +13,10 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        imageLoader = Callhttp() { imageUrl ->
-            Picasso.get().load(imageUrl)
-                .into(binding.commonImage, object : Callback{
-                    override fun onSuccess() {
-                        binding.progressBar.visibility = View.GONE
-                        binding.randomButton.isClickable = true
-                    }
-
-                    override fun onError(e: Exception?) {
-                        binding.progressBar.visibility = View.GONE
-                        binding.randomButton.isClickable = true
-                        Picasso.get().load(R.drawable.placeholder).into(binding.commonImage)
-                    }
-
-                })
-            binding.progressBar.visibility = View.VISIBLE
-            binding.randomButton.isClickable = false
-
-            val text = "Подождите загрузки изображения"
-            val duration = Toast.LENGTH_SHORT
-
-            val toast = Toast.makeText(applicationContext, text, duration)
-            toast.setGravity(Gravity.CENTER, 0, -400)
-            toast.show()
-        }
-
-        imageLoader.loadImage()
-
-        binding.randomButton.setOnClickListener {
-            imageLoader.loadImage()
+        binding.randomDog.setOnClickListener {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.frame_layout, RandomDogFragment()).commit()
         }
     }
 }
---
+++ app/src/main/java/com/example/dogapi/RandomDogFragment.kt
@@ -0,0 +1,61 @@
+package com.example.dogapi
+
+import android.os.Bundle
+import android.view.Gravity
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.dogapi.call.Callhttp
+import com.example.dogapi.databinding.FragmentRandomDogBinding
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+
+private lateinit var binding: FragmentRandomDogBinding
+private lateinit var imageLoader: Callhttp
+
+
+class RandomDogFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRandomDogBinding.inflate(inflater)
+
+        imageLoader = Callhttp() { imageUrl ->
+            Picasso.get().load(imageUrl)
+                .into(binding.commonImage, object : Callback {
+                    override fun onSuccess() {
+                        binding.progressBar.visibility = View.GONE
+                        binding.randomButton.isClickable = true
+                    }
+
+                    override fun onError(e: Exception?) {
+                        binding.progressBar.visibility = View.GONE
+                        binding.randomButton.isClickable = true
+                        Picasso.get().load(R.drawable.placeholder).into(binding.commonImage)
+                    }
+
+                })
+
+            binding.progressBar.visibility = View.VISIBLE
+            binding.randomButton.isClickable = false
+
+            val text = "Подождите загрузки изображения"
+            val duration = Toast.LENGTH_SHORT
+
+            val toast = Toast.makeText(context, text, duration)
+            toast.setGravity(Gravity.CENTER, 0, -400)
+            toast.show()
+        }
+
+        imageLoader.loadImage()
+
+        binding.randomButton.setOnClickListener {
+            imageLoader.loadImage()
+        }
+
+        return binding.root
+    }
+}
